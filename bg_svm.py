31dc782ed3de50cd4bf16d7434ca3151e9f67b32
pratik-kubal <17088230+pratik-kubal@users.noreply.github.com>
2018-11-22 01:32:48 +0000

    Completed SVM , in process of finishing Ensemble methods

--- bg_svm.py
+++ bg_svm.py
@@ -17,17 +17,17 @@ val_data = validation_data[0]
 val_target = validation_data[1]
 test_target = test_data[1]
 test_data = test_data[0]
-classifier1 = SVC(kernel='rbf', C=2, gamma = 0.05,cache_size=7000,verbose=True)
+classifier1 = SVC(kernel='rbf', C=2, gamma = 0.05,cache_size=7000,probability=True,verbose=True)
 classifier1.fit(train_data,train_target)
 from sklearn.externals import joblib
-joblib.dump(classifier1,'./models/SVMSlideModel.joblib',compress=True)
-baseline = SVC(kernel='rbf',gamma=1,cache_size=7024,verbose=True,probability=True)
-baseline
-classifier = baseline
-classifier.fit(train_data,train_target)
-joblib.dump(classifier,'./models/SVMpdfModel2.joblib',compress=True)
-baseline3 = SVC(kernel='rbf',cache_size=7000,verbose=True,probability=True)
-classifier = baseline3
-classifier
-classifier.fit(train_data,train_target)
-joblib.dump(classifier,'./models/SVMpdfModel3.joblib',compress=True)
+joblib.dump(classifier1,'./models/SVMSlideModelProbT.joblib',compress=True)
+#baseline = SVC(kernel='rbf',gamma=1,cache_size=7024,verbose=True,probability=True)
+#baseline
+#classifier = baseline
+#classifier.fit(train_data,train_target)
+#joblib.dump(classifier,'./models/SVMpdfModel2.joblib',compress=True)
+#baseline3 = SVC(kernel='rbf',cache_size=7000,verbose=True,probability=True)
+#classifier = baseline3
+#classifier
+#classifier.fit(train_data,train_target)
+#joblib.dump(classifier,'./models/SVMpdfModel3.joblib',compress=True)
